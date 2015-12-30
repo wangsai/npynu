ac43c7afe7377f58ab335ad9921475f5dec73eab
王赛 <wangsai@bootcss.com>
2015-12-30 21:36:58 +0800

    主要 api 实现文件

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 'use strict';
 
-var upyun = require('./lib/api.js');
-
-module.exports = exports.UPYUN = function(bucket, operator, password, endpoint) {
-
-    return new upyun(bucket, operator, password, endpoint);
-};
+module.exports = exports.UPYUN = require('./lib/api.js');
